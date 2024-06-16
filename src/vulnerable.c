ec73e7e430eb7b83204f4d7597828b61852a8491
Burcea Bogdan-Madalin <burcea.bogdan.madalin@gmail.com>
2024-06-16 16:12:40 +0300

    Add "bin", "src" folders; Add "vulnerable.c" file.

---
+++ src/vulnerable.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+// gcc src/vulnerable.c -o bin/vulnerable.exe && bin/vulnerable.exe "Test Message"
+
+
+void printUserMessageVulnerable(char *msg) {
+    char localBuffer[100] = {};
+    strcpy(localBuffer, msg);
+    printf("User message: %s\n", localBuffer);
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s userMessage\n", argv[0]);
+        exit(-1);
+    }
+
+    printUserMessageVulnerable(argv[1]);
+
+    return 0;
+}
