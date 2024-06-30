e844d23f255d8ba1df570c96adb60fbe5d1d23c7
Burcea Bogdan-Madalin <burcea.bogdan.madalin@gmail.com>
2024-06-30 21:10:56 +0300

    Add "assertMessage()" macro and tweak "exiterror()" macro in "utils.hpp".

--- src/utils.hpp
+++ src/utils.hpp
@@ -8,7 +8,11 @@
 #define pv(x) std::cout<<#x<<" = "<<(x)<<"; ";std::cout.flush()
 #define pn std::cout<<std::endl
 
-#define exiterror(msg) std::cerr << (msg) << std::endl; exit(-1)
+#define exiterror(msg) std::cerr << (msg) << '\n' << "exit(-1);" << std::endl; exit(-1)
+#define assertMessage(condition, msg) \
+    do { \
+        if (!(condition)) { exiterror(msg); } \
+    } while(0)
 
 #define UNUSED(variable) do { (void)(variable); } while (0)
 
